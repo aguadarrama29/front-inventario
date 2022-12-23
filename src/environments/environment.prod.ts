f4c568c293ff6e8cd06acca96c7a01e980317eda
aguadarrama29 <68649948+aguadarrama29@users.noreply.github.com>
2022-12-23 14:08:30 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //es la url que comparten todos los metodos
-  base_url:"http://localhost:8098/api/v1"
+  base_url:"https://front-inventario-git-my-proyecto.apps-crc.testing/api/v1"
 };
