da52b4e244f2f07c96d009bcf1a9b4dc73a8e670
aguadarrama29 <68649948+aguadarrama29@users.noreply.github.com>
2022-12-23 15:31:42 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //es la url que comparten todos los metodos
-  base_url:"https://inventory-my-proyecto.apps-crc.testing/api/v1"
+  base_url:"https://inventory-my-proyecto.apps-crc.testing/api/v1",
+  NODE_DEV=development
 };
